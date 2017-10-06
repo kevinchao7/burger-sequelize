abefef5b3831c2b94bf3bf78564682a65ba12bb2
kevin chao <kchao562@gmail.com>
2017-10-05 21:24:42 -0700

    initial release

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,51 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
-};
+module.exports = (sequelize, DataTypes)=>{
+  const burgers = sequelize.define('burgers',{
+    burger_name :
+      {
+        allowNull : false,
+        type      : DataTypes.STRING,
+        validate  :
+          {
+            len : { args : [1,20], msg : "Must be between 1 and 20 characters."}
+          }
+      },
+    devoured :
+      {
+        type         : DataTypes.BOOLEAN,
+        defaultValue : false
+      }
+  })
+  return burgers;
+}
 
-// Export the database functions for the controller (burgerController.js).
-module.exports = burger;
+
+// // Import the ORM to create functions that will interact with the database.
+// var orm = require("../config/orm.js");
+//
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays.
+//   create: function(cols, vals, cb) {
+//     orm.create("burgers", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   },
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+//
+// // Export the database functions for the controller (burgerController.js).
+// module.exports = burger;
--- server.js
+++ server.js
@@ -2,27 +2,28 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var db = require('./models');
+// db.sequelize.sync({ force : true }); // Forces server to drop db if exists and recreate it.
+db.sequelize.sync();
 
 var app = express();
+var port = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
-
-app.use("/", routes);
+require("./routes/api-routes.js")(app,db);
 
 app.listen(port);
